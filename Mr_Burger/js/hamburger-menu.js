e29248676a76781d2c242315a22d09fd02219205
Liubov Alifanova <lunay17@bk.ru>
2018-01-31 00:39:45 +0300

    commit

---
+++ Mr_Burger/js/hamburger-menu.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	$('.hamburger-menu-link').on('click', function(e) {
+    e.preventDefault()
+
+    $('.hamburger-menu').fadeIn(800);
+
+  });
+  
+	$('.hamburger-menu__close').on('click', function(e) {
+    e.preventDefault()
+
+    $('.hamburger-menu').fadeOut(800);
+
+  });
+})
+
+$(document).ready(function() {
+  
+  $('.ingredients_icon').on('click', function() {
+    
+    $('.ingredients_menu').slideToggle(300);
+  });
+
+});
